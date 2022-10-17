f974a42b4ede609547bb49cc153f7190381e1231
AbuleleMankomo <ntuliniabulele2@gmail.com>
2022-10-17 00:06:58 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets in lower and uppercase
+ * Return: Always 0
+ */
+int main(void)
+	char c;
+
+	char d;
+
+	c = 'a'
+	d = 'd'
+
+	while
+       		(c <= 'z');{
+			putchar(c);
+			c++;
+		}
+	while
+       		(d <= 'Z');{
+			putchar(d);
+			d++;
+		}
+
+	putchar('\n');
+
+	return(0);
+}
