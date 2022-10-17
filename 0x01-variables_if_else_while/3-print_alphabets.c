00fdea64af5563aaf4c3a526bcc6b7522d0c6b8a
AbuleleMankomo <ntuliniabulele2@gmail.com>
2022-10-17 12:34:16 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,25 +4,24 @@
  * Return: Always 0
  */
 int main(void)
+{
 	char c;
 
 	char d;
 
-	c = 'a'
-	d = 'd'
-
+	
 	while
-       		(c <= 'z');{
+		(c <= 'z') {
 			putchar(c);
 			c++;
 		}
 	while
-       		(d <= 'Z');{
+		(d <= 'Z') {
 			putchar(d);
 			d++;
 		}
 
 	putchar('\n');
 
-	return(0);
+	return (0);
 }
