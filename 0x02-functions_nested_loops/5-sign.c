0234fdb9629c055ebf6aa2fdf3eddea48ca0ca03
AbuleleMankomo <ntuliniabulele2@gmail.com>
2022-10-19 14:33:35 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print sign - prints sign of a number
+ * @n: number of a sign tobe printed
+ * Return: 1 if greater than 0
+ * 0 if zero
+ * -1 if less than 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if else(n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+
+
+		return (-1);
+	}
+}
