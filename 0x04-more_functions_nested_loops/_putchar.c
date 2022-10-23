1661847729c2198d6112d611fa5f752c552bdecf
AbuleleMankomo <ntuliniabulele2@gmail.com>
2022-10-23 07:49:49 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _putchar writes the character c to standard output (stdout)
+ * @c - is the character to print
+ * Return on success 1 else -1
+ */
+int _putchar (char c)
+{
+	return (write(1, &c, 1));
+}
