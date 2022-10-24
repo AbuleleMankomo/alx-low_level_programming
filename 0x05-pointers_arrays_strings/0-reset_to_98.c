3cbeeb3abb02a92fca5f09839553787c9d12b18d
AbuleleMankomo <ntuliniabulele2@gmail.com>
2022-10-24 14:27:50 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - to update the value to 99
+ * @n - input
+ * retuen: always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - function to print stdout
+ *
+ * Return: always 0
+ */
+int _putchar(char c)
+{
+	return (write(1 &c 1));
+}
