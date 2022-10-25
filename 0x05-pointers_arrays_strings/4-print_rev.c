b031fd8807946a34e7afd767711e2f24cc4cbf73
AbuleleMankomo <ntuliniabulele2@gmail.com>
2022-10-24 23:37:02 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s - string to be printed
+ */
+void print_rev(char *s)
+{
+	int l = 0;
+
+	while (s[l] != '\0')
+		l++;
+
+	for (l = l - 1; l >= 0; l--)
+		_putchar(s[l]);
+
+	_putchar('\n');
+}
