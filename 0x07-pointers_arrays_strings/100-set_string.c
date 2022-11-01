36a1d8bdcdc7706543e06f9177b577169d932da9
AbuleleMankomo <ntuliniabulele2@gmail.com>
2022-11-01 09:26:02 -0700

    Task 8 set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * void set_string(char **s, char *to)
+ * @s: source adress
+ * @to: target adress.
+ * Return: no return
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
