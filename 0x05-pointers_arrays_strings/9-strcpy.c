d62351deaa9118bfe0cb6efc8b4d55885c10b0ec
AbuleleMankomo <ntuliniabulele2@gmail.com>
2022-11-06 12:50:07 -0800

    Task 10

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,11 +10,15 @@
 char *_strcpy(char *dest, char *src)
 {
 	int len = 0;
+	int i = 0;
 
-	while (*(src + len) != '\0')
+	while (*(src + len))
 	{
-		*(dest + len) = *(src + len);
 		len++;
+	}
+	for ( ; i < len ; i++)
+	{
+		*(dest + i) = *(src + i);
 	}
 	*(dest + len) = '\0';
 	return (dest);
