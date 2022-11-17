8a72cdd29473ec37b2b26a0f3471c0232f0fc6e5
AbuleleMankomo <ntuliniabulele2@gmail.com>
2022-11-17 12:15:56 -0800

    Task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,51 +1,62 @@
 #include "3-calc.h"
 /**
- * op_add - adds two integers
- * @a: first integer
- * @b: second integer
- * Return: sum of two integers
+ * op_add - return the suma of a and b
+ * @a: int type Integer
+ * @b: int type Intger
+ * Return: add
  */
 int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - return the difference of a and b
+ * @a: int type Integer
+ * @b: int type Intger
+ * Return: sub
+ */
+int op_sub(int a, int b)
 {
 	return (a - b);
 }
 /**
- * op_mul - multiply two integers
- * @a: first integer
- * @b: second integer
- * Return: product of two integers
+ * op_mul - return the product of a and b
+ * @a: int type Integer
+ * @b: int type Intger
+ * Return: mul
  */
 int op_mul(int a, int b)
 {
-	return (a * b);
+	int mul = a * b;
+
+	return (mul);
 }
 /**
- *op_div - divides two integers
- * @a: first integer
- * @b: second integer
- * Return: quotient of two integers
+ * op_div - return the division of a and b
+ * @a: int type Integer
+ * @b: int type Intger
+ * Return: div
  */
 int op_div(int a, int b)
 {
-	if (!b)
+	if (b == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	return (a / b);
 }
 /**
- * op_mod - calculates modulus two integers
- * @a: first integer
- * @b: second integer
- * * Return: modulus of two integers
+ * op_mod - return the remainder of the division of a and b
+ * @a: int type Integer
+ * @b: int type Intger
+ * Return: mod
  */
 int op_mod(int a, int b)
 {
-	if (!b)
+	if (b == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-return (a % b);
+	return (a % b);
 }
